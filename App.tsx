25b677c21f42d2349483d512829ce8b4a0fd66d5
Rafael Freitas <rafaelbogfreitas@gmail.com>
2022-07-20 14:11:39 -0300

    Complete signin screen and support components (#2)
    Co-authored-by: Rafael Freitas <rafaelborges1985@hotmail.com>

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import {NativeBaseProvider, StatusBar} from "native-base";
+
+// Fonts
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 // Components
-import SignIn from './src/screens/SignIn';
+import SignIn from "./src/screens/SignIn";
+import Loading from "./src/components/Loading";
+
+// Styles
+import {THEME} from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
-
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {Heading, Icon, useTheme, VStack} from "native-base";
+import {Envelope, Key} from "phosphor-react-native";
+
+// Assets
+import Logo from "../../assets/logo_primary.svg";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const SignIn = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {colors} = useTheme();
+
+  function handleSignIn() {
+    console.log({ name, password });
+  }
+
   return (
-    <View>
-      <Text>Olá Mundo!</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color={"gray.100"} fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta {name}
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setName}
+        />
+
+      <Input
+        type="password"
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={
+          <Icon as={<Key color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setPassword}
+      />
+
+      <Button title="Entrar" w="full" onPress={handleSignIn}/>
+    </VStack>
   );
 };
 
